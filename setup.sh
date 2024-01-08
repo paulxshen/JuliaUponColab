3b792b328d805a9cc00d904c5c2b1981ea831304
Paul Shen <pxshen@alumni.stanford.edu>
2024-01-08 16:40:30 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,38 @@
+set -e
+
+#---------------------------------------------------#
+JULIA_VERSION="1.10.0" # any version ≥ 0.7.0
+JULIA_PACKAGES="IJulia AbbreviatedStackTraces"
+JULIA_PACKAGES_IF_GPU=""
+JULIA_NUM_THREADS=4
+#---------------------------------------------------#
+
+  # Install Julia
+  JULIA_VER=`cut -d '.' -f -2 <<< "$JULIA_VERSION"`
+  echo "Installing Julia $JULIA_VERSION on the current Colab Runtime..."
+  BASE_URL="https://julialang-s3.julialang.org/bin/linux/x64"
+  URL="$BASE_URL/$JULIA_VER/julia-$JULIA_VERSION-linux-x86_64.tar.gz"
+  wget -nv $URL -O /tmp/julia.tar.gz # -nv means "not verbose"
+  tar -x -f /tmp/julia.tar.gz -C /usr/local --strip-components 1
+  rm /tmp/julia.tar.gz
+
+  # Install Packages
+  if [ "$COLAB_GPU" = "1" ]; then
+      JULIA_PACKAGES="$JULIA_PACKAGES $JULIA_PACKAGES_IF_GPU"
+  fi
+  for PKG in `echo $JULIA_PACKAGES`; do
+    echo "Installing Julia package $PKG..."
+    julia -e 'using Pkg; pkg"add '$PKG'; precompile;"' &> /dev/null
+  done
+
+  # Install kernel and rename it to "julia"
+  echo "Installing IJulia kernel..."
+  julia -e 'using IJulia; IJulia.installkernel("julia", env=Dict(
+      "JULIA_NUM_THREADS"=>"'"$JULIA_NUM_THREADS"'"))'
+  KERNEL_DIR=`julia -e "using IJulia; print(IJulia.kerneldir())"`
+  KERNEL_NAME=`ls -d "$KERNEL_DIR"/julia*`
+  mv -f $KERNEL_NAME "$KERNEL_DIR"/julia
+
+  echo ''
+  echo "Successfully installed `julia -v`!"
+  echo "Please reload this page (press Ctrl+R, ⌘+R, or the F5 key) then change runtime to Julia "
